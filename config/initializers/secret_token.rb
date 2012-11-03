4cbdc1da644b0e8d6664f40e7e1d5d7327e10076
aizawa1126 <aizawa_tatsuya@yahoo.co.jp>
2012-11-03 09:18:49 +0900

    rails new cheers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cheers::Application.config.secret_token = 'f45d249b7003f90b4b5174e85ab250913f3dcf3a76b6224f7210cfb9c96c1b20eab76b94463b5f620c96d4da59b6e33d2b085ea3e9ae57a2690f3544a16fde0a'
