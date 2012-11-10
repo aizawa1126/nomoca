c193405ce543dde9d96e0ae22a96f4ef048acda3
aizawa1126 <aizawa_tatsuya@yahoo.co.jp>
2012-11-11 00:06:20 +0900

    add start time

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ActiveRecord::Base
   belongs_to :user
   has_many :invitations, :dependent => :destroy
 
-  attr_accessible :budget, :context, :date, :place, :title, :user_id, :public
+  attr_accessible :budget, :context, :date, :place, :title, :user_id, :public, :start_time
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 8) do
+ActiveRecord::Schema.define(:version => 9) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 8) do
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
     t.string   "public"
+    t.datetime "start_time"
   end
 
   create_table "invitations", :force => true do |t|
